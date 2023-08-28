476412bfd901aa0967ad193d9baa9984167eb9cc
Faith Achieng <faith.achieng@student.moringaschool.com>
2023-08-28 07:17:30 +0300

    Complted Assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,17 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>
+        I'm a passionate web developer with a strong interest in creating
+        innovative and user-friendly websites. My goal is to leverage my skills
+        to build meaningful online experiences.
+      </p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
